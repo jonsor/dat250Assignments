038ff6423367fa71be7b4a40c09deaf0a9e12762
Jonas <jsorsdal@gmail.com>
2019-08-27 13:20:21 +0200

    Initial commit

---
+++ Assignment1/src/main/java/entities/Address.java
@@ -0,0 +1,9 @@
+package entities;
+
+public class Address {
+
+	private String street;
+	private String city;
+	private String zip;
+	
+}
---
+++ Assignment1/src/main/java/entities/User.java
@@ -0,0 +1,9 @@
+package entities;
+
+public class User {
+
+	private String uname;
+	private String fname;
+	private String Iname;
+	private String password;
+}
