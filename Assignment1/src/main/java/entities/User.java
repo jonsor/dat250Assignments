3753addce2e5ba1ed411f48d3c9092870ddaf403
Jonas <jsorsdal@gmail.com>
2019-08-27 13:53:20 +0200

    Stuff added to Adress and User

--- Assignment1/src/main/java/entities/User.java
+++ Assignment1/src/main/java/entities/User.java
@@ -1,9 +1,80 @@
 package entities;
 
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
 public class User {
 
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    
 	private String uname;
 	private String fname;
 	private String Iname;
 	private String password;
+	
+    @OneToMany(mappedBy="user")
+    @OrderBy("name ASC")
+	private List<Address> adresses;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getIname() {
+		return Iname;
+	}
+
+	public void setIname(String iname) {
+		Iname = iname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Collection<Address> getAdresses() {
+		return adresses;
+	}
+
+	public void setAdresses(List<Address> adresses) {
+		this.adresses = adresses;
+	}
+    
+    
+	
 }
