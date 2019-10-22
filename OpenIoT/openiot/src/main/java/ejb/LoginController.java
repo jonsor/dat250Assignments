06ae5ee6f314ddff3c045a7cea087775eb6e441c
Sander Breivik <sander@acos.no>
2019-10-22 16:07:03 +0200

    done stuff i do not remember

---
+++ OpenIoT/openiot/src/main/java/ejb/LoginController.java
@@ -0,0 +1,16 @@
+package ejb;
+
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+
+@Named(value = "loginController")
+@RequestScoped
+public class LoginController implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	
+
+}
